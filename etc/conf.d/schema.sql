9ba8ed9649f6a6463049771743641a0dbdc5cc8f
republichenko <republichenko@gmail.com>
2024-09-03 20:44:30 +0200

    create schema.sql

---
+++ etc/conf.d/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS kafka_queue
+(
+    name String,
+    surname String
+) Engine = Kafka
+SETTINGS
+kafka_broker_list='kafka:19092',
+kafka_topic_list='people',
+kafka_group_name='clickhouse_consumer1',
+kafka_format='JSONEachRow';
+
+CREATE TABLE IF NOT EXISTS kafka_storage
+(
+    name String,
+    surname String
+) Engine = MergeTree;
+
+CREATE MATERIALIZED VIEW kafka_view TO kafka_storage AS SELECT name, surname FROM kafka_queue;
